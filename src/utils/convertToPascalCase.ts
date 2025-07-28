148a310787e03432b48699a43d4be7ec52ee9e76
Pariola <kehindephilip15@gmail.com>
2025-07-29 00:18:50 +0100

    refactor(convertToPascalCase): improve string conversion logic
    refactored the convertToPascalCase function to enhance readability and maintainability. The new implementation converts the input text to lowercase, removes unwanted characters, and capitalizes the first letter of each word.

--- src/utils/convertToPascalCase.ts
+++ src/utils/convertToPascalCase.ts
@@ -1,3 +1,6 @@
 export const convertToPascalCase = (text: string): string => {
-    return text.replace(/(^\w|-\w|\s\w)/g, (c) => c.replace(/[- ]/, '').toUpperCase());
+    return text
+        .toLowerCase()
+        .replace(/[^a-z0-9\s-]/gi, '')
+        .replace(/(?:^|[\s-])(\w)/g, (_, c: string) => c.toUpperCase());
 };
